1b11038e944482cb9ffd09a5d53372227f0aa592
Ashersam <ashersamuel06@gmail.com>
2020-01-13 13:38:25 +0530

    added build

---
+++ build/precache-manifest.f3ed176dd4cbcb7aa7a2dc143660ad31.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad5e2c511243042c48bee65886bef8d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8d0dea4ab3e0d0d3eec8",
+    "url": "/static/js/2.303204e9.chunk.js"
+  },
+  {
+    "revision": "7c441fd7c9602b1a5f47",
+    "url": "/static/js/main.23244fbe.chunk.js"
+  },
+  {
+    "revision": "86e525d7c77477a8569e",
+    "url": "/static/js/runtime-main.041cbe29.js"
+  }
+]);

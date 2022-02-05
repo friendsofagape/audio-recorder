84d3a81b8a22ffd1cbda14a4f1dc2fb85c642c5a
Ashersam <ashersamuel06@gmail.com>
2022-02-05 20:05:48 +0530

    save local files

--- src/core/saveToLocalforage.js
+++ src/core/saveToLocalforage.js
@@ -3,9 +3,9 @@ import localforage from "localforage"
 const saveToLocalforage = ({ verse, version, verseBatchNum, blobUrl  }) => {
     console.log("dsdsdsdds")
   const savetoLocal = [{
-    [verse]: {
+    [verse]: [{
         [version]: [{[verseBatchNum]: blobUrl}],
-    }
+    }]
   }]
     localforage.getItem('verseData', function (err, value) {
         // if err is non-null, we got an error. otherwise, value is the value
@@ -17,13 +17,25 @@ const saveToLocalforage = ({ verse, version, verseBatchNum, blobUrl  }) => {
                         Object.entries(val).forEach(([versenum, verseVal]) => {
                             if(versenum === verse){
                                 console.log("same value")
+                                Object.entries(verseVal).forEach(([keyval, versions ]) => {
+                                    Object.entries(versions).forEach(([batch, bachnum ]) => {
+                                    console.log(batch,"batch")
+                                        if(batch === version ){
+                                            console.log(bachnum.push({[verseBatchNum]:  blobUrl }))
+                                        }else{
+                                            verseVal.push({[version]: [{[verseBatchNum]: blobUrl}]})
+                                            console.log(verseVal)
+                                        }
+                                    })
+                                })
+                                
                             } else{
                                 console.log(`${versenum} ${verseVal}`);
                                 if(versenum !== verse) {
                                     value.push({
-                                        [verse]: {
+                                        [verse]: [{
                                             [version]: [{[verseBatchNum]: blobUrl}],
-                                        }
+                                        }]
                                     })
                                 }
                             }
